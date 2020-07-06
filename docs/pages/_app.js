4414404b3683256e1c7c709a7941b052aa203493
Andrew Lisowski <lisowski54@gmail.com>
2020-07-06 13:09:15 -0700

    basic ignite setup

---
+++ docs/pages/_app.js
@@ -0,0 +1,21 @@
+import React from "react";
+import App from "next/app";
+import { MDXProvider } from "@mdx-js/react";
+import { igniteComponents } from "next-ignite";
+
+import "prismjs/themes/prism.css";
+import "next-ignite/dist/main.css";
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <MDXProvider components={igniteComponents}>
+        <Component {...pageProps} />
+      </MDXProvider>
+    );
+  }
+}
+
+export default MyApp;
